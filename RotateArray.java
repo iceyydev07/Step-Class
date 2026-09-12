a85ee68c980db207e62fa0588e4435c3e9f21abb
arnavv <109091030+iceyydev07@users.noreply.github.com>
2026-09-12 22:23:02 +0530

    Add files via upload

---
+++ RotateArray.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class RotateArray {
+
+    static int[] rotateArray(int[] nums, int k) {
+        k = k % nums.length;
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int newPosition = (i + k) % nums.length;
+            result[newPosition] = nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        int[] result = rotateArray(nums, k);
+
+        System.out.println("Rotated Array:");
+        System.out.println(Arrays.toString(result));
+    }
+}
