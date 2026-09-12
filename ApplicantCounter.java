8e4dd6e89c7ef5fbe4a02262bcce9cee01c30893
arnavv <109091030+iceyydev07@users.noreply.github.com>
2026-09-12 22:16:12 +0530

    Add files via upload

---
+++ ApplicantCounter.java
@@ -0,0 +1,17 @@
+public class ApplicantCounter {
+    static class Applicant {
+        static int totalApplicants = 0;
+
+        Applicant() {
+            totalApplicants++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Applicant applicant1 = new Applicant();
+        Applicant applicant2 = new Applicant();
+        Applicant applicant3 = new Applicant();
+
+        System.out.println("Total applicants: " + Applicant.totalApplicants);
+    }
+}
